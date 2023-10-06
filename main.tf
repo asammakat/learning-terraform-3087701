0c13235b563bac459b78dae9d9eb48952b331f65
Adam Sammakia <adam.sammakia@gmail.com>
2023-10-06 18:06:10 -0500

    more fixes

--- main.tf
+++ main.tf
@@ -33,7 +33,7 @@ resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
-  vpc_id = module.vpc.public_subnets[0]
+  subnet_id = module.vpc.public_subnets[0]
   vpc_security_group_ids = [module.blog_sg.security_group_id]
 
   tags = {
@@ -44,8 +44,9 @@ resource "aws_instance" "blog" {
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws" # module code defined here
   version = "5.1.0"
-  name = "blog"
 
+  vpc_id  = module.blog_vpc.vpc_id
+  name = "blog"
   ingress_rules       = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
   egress_rules       = ["all-all"]
