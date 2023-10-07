60965ca567a7c1e85cdb8d87b58e8526049b09f5
Adam Sammakia <adam.sammakia@gmail.com>
2023-10-07 08:52:16 -0500

    whitespace error

--- variables.tf
+++ variables.tf
@@ -33,12 +33,12 @@ variable "environment "{
   }
 }
 
-variable "asg_min_size " {
+variable "asg_min_size" {
   description = "minimum number of instances in the ASG"
   default     = 1
 }
 
-variable "asg_max_size " {
+variable "asg_max_size" {
   description = "maximum number of instances in the ASG"
   default     = 2
 }
